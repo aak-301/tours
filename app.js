99f90bd489e38d9965a2dc4732664ac097f41224
aakash-301 <aakashshivanshu5@gmail.com>
2023-08-28 21:08:23 +0530

    env. setup for dev and prod.

--- app.js
+++ app.js
@@ -6,10 +6,11 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // MIDDLEWARES
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
-
-app.use(morgan('dev'));
-
+app.use(express.static(`${__dirname}/public`));
 // ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
