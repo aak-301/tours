36f7fffad8e95e2c39518712ca123b818438198b
aakash-301 <aakashshivanshu5@gmail.com>
2023-08-29 00:10:28 +0530

    added mongodb withe mongoose driver

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORRD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connection successfull'));
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
