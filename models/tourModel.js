47d8fcc7aee9ca7d0759ae6a0999b2b9a21e2e65
aakash-301 <aakashshivanshu5@gmail.com>
2023-08-31 23:19:48 +0530

    feat(models):added validators

--- models/tourModel.js
+++ models/tourModel.js
@@ -8,6 +8,8 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'A tour must have a name'],
       unique: true,
       trim: true,
+      maxlength: [40, 'A tour name must have less or equal then 40 charecters'],
+      minlength: [10, 'A tour name must have at least 10 charecters'],
     },
     slug: String,
     duration: {
@@ -21,8 +23,17 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, 'A tour must have a difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either easy, medium, difficult',
+      },
     },
-    ratingsAverage: { type: Number, default: 4.5 },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+      min: [1, 'Rating mst be above 1'],
+      max: [5, 'Raing must be below 5'],
+    },
     ratingsQuantity: { type: Number, default: 0 },
     price: { type: Number, required: [true, 'A tour must have a price'] },
     priceDiscount: Number,
