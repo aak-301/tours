09534c08fb61fd442d4445f4547ea6e9649afe43
aakash-301 <aakashshivanshu5@gmail.com>
2023-09-09 17:26:15 +0530

    fix: restricted user to update roles directly

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,16 @@ const User = require('../models/userModel');
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) {
+      newObj[el] = obj[el];
+    }
+  });
+  return newObj;
+};
+
 exports.getAllUsers = catchAsync(async (req, res) => {
   const users = await User.find();
 
@@ -12,7 +22,7 @@ exports.getAllUsers = catchAsync(async (req, res) => {
   });
 });
 
-exports.updateMe = (req, res, next) => {
+exports.updateMe = catchAsync(async (req, res, next) => {
   //1) Create user if user POSTs password data
   if (req.body.password || req.body.passwordConfirm) {
     return next(
@@ -23,11 +33,22 @@ exports.updateMe = (req, res, next) => {
     );
   }
 
-  //2) Update user doc
+  //2) Filtered out the unwanted name which are not allowed
+  const filteredBody = filterObj(req.body, 'name', 'email');
+
+  //3) Update user doc
+  const updateUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
   res.status(200).json({
     status: ' sucess',
+    data: {
+      user: updateUser,
+    },
   });
-};
+});
 
 exports.getUser = (req, res) => {
   res.status(500).json({
