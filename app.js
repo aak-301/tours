7f0f5dde70c1e56405cf501e3c2e7ac212029777
aakash-301 <aakashshivanshu5@gmail.com>
2023-09-09 18:05:05 +0530

    feat(rate-limit):added rate limit of 100 req/ hr

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const globalErrorHandler = require('./controllers/errorController');
@@ -7,10 +9,19 @@ const AppError = require('./utils/appError');
 
 const app = express();
 
-// MIDDLEWARES
+// GLOBAL MIDDLEWARES
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+const limitter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many reuqest from this IP, please try again in an hour',
+});
+
+app.use('/api', limitter);
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 app.use((req, res, next) => {
