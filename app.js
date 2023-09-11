7bcb97453d6ab1edd4baf91078fe8bbed0f289a3
aakash-301 <aakashshivanshu5@gmail.com>
2023-09-11 21:12:53 +0530

    feat(review):added contoller and routes for review

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const hpp = require('hpp');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
 const globalErrorHandler = require('./controllers/errorController');
 const AppError = require('./utils/appError');
 
@@ -65,6 +66,7 @@ app.use((req, res, next) => {
 // ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
 
 // UNHANDLED ROUTE
 app.all('*', (req, res, next) => {
