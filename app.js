14603775855d2067c691727a8846307c9915229f
aakash-301 <aakashshivanshu5@gmail.com>
2023-09-14 19:25:27 +0530

    feat:added central header

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const rateLimit = require('express-rate-limit');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const path = require('path');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -13,6 +14,12 @@ const globalErrorHandler = require('./controllers/errorController');
 const AppError = require('./utils/appError');
 
 const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', path.join(`${__dirname}`, 'views'));
+
+// Serving static files
+app.use(express.static(path.join(`${__dirname}`, 'public')));
 
 // GLOBAL MIDDLEWARES
 
@@ -55,15 +62,19 @@ app.use(
   })
 );
 
-// Serving static files
-app.use(express.static(`${__dirname}/public`));
-
 app.use((req, res, next) => {
   // console.log(req.headers);
   next();
 });
 
 // ROUTES
+app.get('/', (req, res) => {
+  res.status(200).render('base', {
+    tour: 'The Forest Hiker',
+    user: 'Jonas',
+  });
+});
+
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
